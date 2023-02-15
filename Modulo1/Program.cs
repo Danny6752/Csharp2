91f0c21dceba80484c13e46ec21729ae27f28670
Danny6752 <danitatin1900@gmail.com>
2023-02-15 15:01:36 -0500

    Variables

---
+++ Modulo1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("1");
+Console.WriteLine("2");
+
+int edad = 5;
+Console.WriteLine(edad);
+Console.WriteLine("ingrese su edad")
+
+bool bandera = false;
+Console.WriteLine(bandera);
+
+var num = 1;
+Console.WriteLine(num);
+
+string mensaje = "hola";
+Console.WriteLine("Dime tu nombre");
+Console.WriteLine(mensaje);
+Console.WriteLine("Hello, World!");
+
+int numeroSuperGrande = int.MaxValue;
+Console.WriteLine(numeroSuperGrande);
+
+float SuperGrande = 180,12344f; // ocupa 4 bytes;
+
+float SuperGrande = float.MaxValue;
+Console.WriteLine(SuperGrande);
+
+double Grande = 45.236324 // ocupa 8 bytes; se usa para enteros y decimales.
+
+double Grande = double.MaxValue;
+Console.WriteLine(Grande);
+
+decimal monto = 9.99m; // ocupa 16 bytes; 28 a 29 bytes de precisión.
+
+// determinar el max valor de float, decimal y double.
